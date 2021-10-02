2579209d58c48f31548c68cc55c15e6e56c26e9e
RileDev <rileristic15@outlook.com>
2021-10-02 12:24:33 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace passwordGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.nudCharLenght = new System.Windows.Forms.NumericUpDown();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCharLenght)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.btnSave);
+            this.pnlMain.Controls.Add(this.nudCharLenght);
+            this.pnlMain.Controls.Add(this.txtPassword);
+            this.pnlMain.Controls.Add(this.btnGenerate);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Padding = new System.Windows.Forms.Padding(5);
+            this.pnlMain.Size = new System.Drawing.Size(384, 161);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // nudCharLenght
+            // 
+            this.nudCharLenght.Location = new System.Drawing.Point(216, 42);
+            this.nudCharLenght.Name = "nudCharLenght";
+            this.nudCharLenght.Size = new System.Drawing.Size(45, 20);
+            this.nudCharLenght.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(123, 82);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ReadOnly = true;
+            this.txtPassword.Size = new System.Drawing.Size(151, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(135, 40);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "btnGenerate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(132, 117);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(129, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "btnSave";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCharLenght)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.NumericUpDown nudCharLenght;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace passwordGenerator
+{
+    public partial class Form1 : Form
+    {
+        private Random _rand;
+        private char[] _char = { 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private StringBuilder _passBuilder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            _rand = new Random();
+            nudCharLenght.Minimum = 6;
+            nudCharLenght.Maximum = 20;
+            btnGenerate.Text = "Generate";
+            btnSave.Text = "Save password";
+            this.Text = "P@ssw0rd Generator by: R1lE";
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string password = generatePassword();
+            txtPassword.Text = password;
+        }
+
+        private string generatePassword()
+        {
+            string password;
+            _passBuilder = new StringBuilder();
+
+            for (int i = 0; i < nudCharLenght.Value; i++)
+            {
+                _passBuilder.Append(_char[_rand.Next(_char.Length)]);
+            }
+
+            password = _passBuilder.ToString();
+            return password;
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtPassword.Text))
+            {
+                Clipboard.SetText(txtPassword.Text);
+                MessageBox.Show("Password copied!");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtPassword.Text))
+            {
+                string dir = Directory.GetCurrentDirectory();
+                string fileName = Path.Combine(dir, "passwords.txt");
+                if (!File.Exists(fileName))
+                {
+                    File.CreateText(fileName);
+                }
+                else
+                {
+                    writePassword(fileName);
+                }
+
+            }
+        }
+
+        private void writePassword(string fileName)
+        {
+            int numOfLines = File.ReadAllLines(fileName).Length;
+            if (!File.ReadAllLines(fileName).Contains($"Password no.{numOfLines - 1} - {txtPassword.Text}"))
+            {
+                try
+                {
+                    using (StreamWriter sw = new StreamWriter(fileName, Enabled))
+                    {
+                        sw.WriteLine($"Password no.{numOfLines} - {txtPassword.Text}");
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Password: {txtPassword.Text} has already saved!");
+            }
+            
+        }
+    }
+}
